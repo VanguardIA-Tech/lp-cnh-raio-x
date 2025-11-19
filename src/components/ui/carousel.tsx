d57f9cddd22304d9810411b1c78ccf5e23afa7f6
Victor Meireles <victoraamlima@gmail.com>
2025-11-19 14:07:58 -0300

    Refactor testimonials section: replace carousel implementation with TestimonialsCarousel component and update testimonials data structure

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -206,7 +206,7 @@ const CarouselPrevious = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute  h-8 w-8 rounded-full",
+        "absolute p-[2px] rounded-full bg-metal-green",
         orientation === "horizontal"
           ? "-left-12 top-1/2 -translate-y-1/2"
           : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
@@ -216,8 +216,10 @@ const CarouselPrevious = React.forwardRef<
       onClick={scrollPrev}
       {...props}
     >
-      <ArrowLeft className="h-4 w-4" />
-      <span className="sr-only">Previous slide</span>
+      <span className="flex h-full w-full items-center justify-center rounded-full bg-black/80">
+        <ArrowLeft className="h-4 w-4 text-white" />
+        <span className="sr-only">Previous slide</span>
+      </span>
     </Button>
   )
 })
@@ -235,7 +237,7 @@ const CarouselNext = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute h-8 w-8 rounded-full",
+        "absolute p-[2px] rounded-full bg-metal-green",
         orientation === "horizontal"
           ? "-right-12 top-1/2 -translate-y-1/2"
           : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
@@ -245,8 +247,10 @@ const CarouselNext = React.forwardRef<
       onClick={scrollNext}
       {...props}
     >
-      <ArrowRight className="h-4 w-4" />
-      <span className="sr-only">Next slide</span>
+      <span className="flex h-full w-full items-center justify-center rounded-full bg-black/80">
+        <ArrowRight className="h-4 w-4 text-white" />
+        <span className="sr-only">Next slide</span>
+      </span>
     </Button>
   )
 })
